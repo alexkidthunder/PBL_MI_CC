85bd111b35a38bb99d3231a1623f96855f9a3d90
Alexandre Ribeiro Carneiro <ribeiroo.grande@gmail.com>
2021-11-17 22:04:11 -0300

    Criação das classes Aresta e Vértice
    Criação das classes Aresta e Vértice no sistema

--- src/main/java/util/Aresta.java
+++ src/main/java/util/Aresta.java
@@ -5,10 +5,57 @@
  */
 package util;
 
+import java.util.Objects;
+
 /**
  *
  * @author ribei
  */
 public class Aresta {
-    
+
+    private Vertice origem;
+    private Vertice destino;
+
+    public Aresta(Vertice origem, Vertice destino) {
+        this.origem = origem;
+        this.destino = destino;
+    }
+
+    public Vertice getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(Vertice origem) {
+        this.origem = origem;
+    }
+
+    public Vertice getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Vertice destino) {
+        this.destino = destino;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Aresta other = (Aresta) obj;
+        if (!Objects.equals(this.origem, other.origem)) {
+            return false;
+        }
+        if (!Objects.equals(this.destino, other.destino)) {
+            return false;
+        }
+        return true;
+    }
+
 }
