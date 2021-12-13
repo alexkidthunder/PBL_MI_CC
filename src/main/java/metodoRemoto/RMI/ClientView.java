e47960b848b803b0c32655475a6e864f686e4ed0
Biancasantana1 <50915572+Biancasantana1@users.noreply.github.com>
2021-12-13 11:01:43 -0300

    rota bahia a rio grande do norte

--- src/main/java/metodoRemoto/RMI/ClientView.java
+++ src/main/java/metodoRemoto/RMI/ClientView.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package metodoRemoto.testeRMI;
+package metodoRemoto.RMI;
+
+
 
 import controller.GrafoController;
 import java.rmi.NotBoundException;
@@ -15,6 +12,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultListModel;
+import metodoRemoto.RMI.adder;
 import model.Caminho;
 import util.Aresta;
 import util.Vertice;
